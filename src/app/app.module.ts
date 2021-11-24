08d70606e0d51da72b5ee74439f3a779d4f55a03
tadpoleann <laesybab@gmail.com>
2021-11-24 15:12:46 -0800

    generated movie-card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,12 +16,14 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserRegistrationFormComponent,
     UserLoginFormComponent,
+    MovieCardComponent,
     // UserLoginFormComponent
   ],
   imports: [
--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -42,5 +42,4 @@ export class UserRegistrationFormComponent implements OnInit {
       });
     });
   }
-
 }
