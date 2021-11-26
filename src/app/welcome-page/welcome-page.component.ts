fae384031e4edb1d7eb5ce03adb23f8af97577e0
tadpoleann <laesybab@gmail.com>
2021-11-26 02:49:26 -0800

    biting animation works now, loading js file works now

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,26 +1,27 @@
-import { Inject, AfterViewInit, ElementRef, Component, OnInit } from '@angular/core';
+import { Inject, Component, OnInit } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from '../user-login-form/user-login-form.component';
 import { MatDialog } from '@angular/material/dialog';
 
-declare function shit(): void;
+// testing
+declare function test1(): void;
+// declare function poop(): void;
+declare function Animate(): void; 
 
 @Component({
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.component.html',
   styleUrls: ['./welcome-page.component.scss']
 })
-export class WelcomePageComponent implements OnInit, AfterViewInit {
-  constructor(@Inject(DOCUMENT) private document: any, private elementRef: ElementRef, public dialog: MatDialog) {
-  }
-  ngOnInit(): void {
-  }
-
-  ngAfterViewInit(){
-
+export class WelcomePageComponent implements OnInit {
+  constructor(public dialog: MatDialog) {
+    setInterval("Animate()", 400);
+    test1();
   }
 
+  ngOnInit(){
+  }
   openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {
       width: '280px'
--- src/assets/test.js
+++ src/assets/test.js
@@ -1,12 +1,28 @@
-let images = new Array();
-images = ["../assets/first.svg", "../assets/second.svg", "../assets/third.svg"];
-let x = 0;
+// let diarrhea = ["./assets/first.png", "./assets/first.png", "./assets/first.png"];
 
-function shit(){
-    console.log("shit");
-    setInterval(() => {
-        document.getElementById("logo").src = images[x];
-        x++;
-        if(x === images.length){ x = 0;}
-    }, 400);
+// setInterval("Animate()", 400);
+// var x = 0;
+
+// function Animate() {
+//     document.getElementById("logo").src = diarrhea[x];
+//     x++;
+//     if (diarrhea.length == x) {
+//         x = 0;
+//     }
+// }
+
+// alert('test worked');
+// console.log("...")
+let images = ["./assets/first.png", "./assets/second.png", "./assets/third.png"];
+let x = 0;
+function Animate() {
+    document.getElementById("logo").src = images[x];
+    x++;
+    if (images.length == x) {
+        x = 0;
+    }
+}
+
+function test1(){
+    console.log('Calling test 1 function');
 }
