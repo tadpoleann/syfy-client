2b4dc4516a9fa2f198cf3df3bf9143248f16e150
tadpoleann <laesybab@gmail.com>
2021-11-26 19:59:17 -0800

    change button color

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public snackbar: MatSnackBar) { }
 
   ngOnInit(): void {
   }
