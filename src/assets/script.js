89b745de51919da1f4f26f55fccf17a34955a376
tadpoleann <laesybab@gmail.com>
2021-12-06 20:48:42 -0800

    removed last frame on welcom screen

--- src/assets/script.js
+++ src/assets/script.js
@@ -1,9 +1,15 @@
-let images = ["./assets/first.svg", "./assets/second.svg", "./assets/third.svg", "./assets/fourth.svg", "./assets/fifth.svg", "./assets/Big_chungus.png"];
+let images = [
+  "./assets/first.svg",
+  "./assets/second.svg",
+  "./assets/third.svg",
+  "./assets/fourth.svg",
+  "./assets/fifth.svg",
+];
 let x = 0;
 function Animate() {
-    document.getElementById("logo").src = images[x];
-    x++;
-    if (x === images.length) {
-        x = 0;
-    }
+  document.getElementById("logo").src = images[x];
+  x++;
+  if (x === images.length) {
+    x = 0;
+  }
 }
