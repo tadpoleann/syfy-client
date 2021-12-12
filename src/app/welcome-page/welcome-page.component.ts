ae4f25144f7c95b5c943c03f91519e2ca0f73f3e
tadpoleann <laesybab@gmail.com>
2021-12-12 05:30:38 -0800

    test..

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -4,28 +4,23 @@ import { UserRegistrationFormComponent } from '../user-registration-form/user-re
 import { UserLoginFormComponent } from '../user-login-form/user-login-form.component';
 import { MatDialog } from '@angular/material/dialog';
 
-declare function Animate(): void; 
-
 @Component({
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.component.html',
-  styleUrls: ['./welcome-page.component.scss']
+  styleUrls: ['./welcome-page.component.scss'],
 })
 export class WelcomePageComponent implements OnInit {
-  constructor(public dialog: MatDialog) {
-    setInterval("Animate()", 400);
-  }
-  ngOnInit(){
-  }
+  constructor(public dialog: MatDialog) {}
+  ngOnInit() {}
   openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {
-      width: '280px'
+      width: '280px',
     });
   }
 
-  openUserLoginDialog(): void{
+  openUserLoginDialog(): void {
     this.dialog.open(UserLoginFormComponent, {
-      width: '280px'
+      width: '280px',
     });
   }
 }
