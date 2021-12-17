6c77ddebdd00adeeeb0a8ab89232ecff521f5dad
tadpoleann <laesybab@gmail.com>
2021-12-16 21:37:02 -0800

    created typedoc comments

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 // this import is for closing the dialog on success
-import { MatDialogRef } from '@angular/material/dialog'
+import { MatDialogRef } from '@angular/material/dialog';
 //this import is bringing in the API calls created in fetch-api-data.service.ts
 import { UserRegistrationService } from '../fetch-api-data.service';
 //this import is used to display notifs back to the user
@@ -10,39 +10,41 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-user-registration-form',
   templateUrl: './user-registration-form.component.html',
-  styleUrls: ['./user-registration-form.component.scss']
+  styleUrls: ['./user-registration-form.component.scss'],
 })
 export class UserRegistrationFormComponent implements OnInit {
   isLoading = false;
   //define an input, or the user data
-  @Input() userData = { Username: '', Password: '', Email: '', Birthday: ''};
+  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
 
   constructor(
     public fetchApiData: UserRegistrationService,
     public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
     public snackBar: MatSnackBar
-  ) { }
+  ) {}
 
   //called once the component has received all inputs
-  ngOnInit(): void {
-  }
-
-  //function responsible for sending the form inputs to the backend
+  ngOnInit(): void {}
+  /**
+   * function responsible for sending the form inputs to the backend to create a new user
+   */
   registerUser(): void {
     this.isLoading = true;
-    this.fetchApiData.userRegistration(this.userData).subscribe((response) => {
-  // Logic for a successful user registration (To be implemented)
-     this.dialogRef.close(); // This will close the modal on success
-     console.log(response);
-     this.isLoading = false;
-     this.snackBar.open('user registered successfully', 'OK', {
-        duration: 2000
-     });
-    }, (response) => {
-      console.log(response);
-      this.snackBar.open(response, 'OK', {
-        duration: 2000
-      });
-    });
+    this.fetchApiData.userRegistration(this.userData).subscribe(
+      (response) => {
+        this.dialogRef.close(); // This will close the modal on success
+        console.log(response);
+        this.isLoading = false;
+        this.snackBar.open('user registered successfully', 'OK', {
+          duration: 2000,
+        });
+      },
+      (response) => {
+        console.log(response);
+        this.snackBar.open(response, 'OK', {
+          duration: 2000,
+        });
+      }
+    );
   }
 }
