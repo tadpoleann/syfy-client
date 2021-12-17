1a0b4ff2bc6f079d03318e566a6789c2f74477f6
tadpoleann <laesybab@gmail.com>
2021-12-16 21:09:08 -0800

    created typedoc comments

--- src/app/movie-synopsis/movie-synopsis.component.ts
+++ src/app/movie-synopsis/movie-synopsis.component.ts
@@ -4,10 +4,14 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 @Component({
   selector: 'app-movie-synopsis',
   templateUrl: './movie-synopsis.component.html',
-  styleUrls: ['./movie-synopsis.component.scss']
+  styleUrls: ['./movie-synopsis.component.scss'],
 })
-export class MovieSynopsisComponent{
-
+export class MovieSynopsisComponent {
+  /**
+   * Injects movie details from movie-card component
+   * Gets used in movie-synopsis component dialog
+   * @param data object with strings Title, Description, TrailerUrl, ReleaseYear
+   */
   constructor(
     @Inject(MAT_DIALOG_DATA)
     public data: {
@@ -16,9 +20,7 @@ export class MovieSynopsisComponent{
       TrailerUrl: string;
       ReleaseYear: number;
     }
-  ) { }
+  ) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
