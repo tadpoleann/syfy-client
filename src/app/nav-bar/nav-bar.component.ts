79f260da891b66ca5ef6af790a1bb00156a8af1a
tadpoleann <laesybab@gmail.com>
2021-12-16 21:10:27 -0800

    created typedoc comments

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -4,20 +4,20 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent implements OnInit {
+  constructor(public snackbar: MatSnackBar) {}
 
-  constructor(public snackbar: MatSnackBar) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
-  logOut(): void{
+  /**
+   * logOut function clears username and token from local storage to logout the user.
+   */
+  logOut(): void {
     this.snackbar.open('Successfully logged out', 'OK', {
-      duration:3000
-    })
+      duration: 3000,
+    });
     localStorage.clear();
   }
-
 }
