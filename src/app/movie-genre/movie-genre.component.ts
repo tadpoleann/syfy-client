e3c62492cbfe61c6db549578bbd268cef63a438d
tadpoleann <laesybab@gmail.com>
2021-12-16 21:04:41 -0800

    created typedoc comments

--- src/app/movie-genre/movie-genre.component.ts
+++ src/app/movie-genre/movie-genre.component.ts
@@ -4,17 +4,20 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 @Component({
   selector: 'app-movie-genre',
   templateUrl: './movie-genre.component.html',
-  styleUrls: ['./movie-genre.component.scss']
+  styleUrls: ['./movie-genre.component.scss'],
 })
-export class MovieGenreComponent{
-
+export class MovieGenreComponent {
+  /**
+   * Injects genre Name and Description from movie-card component
+   * Gets used in movie-genre component dialog
+   * @param data object with strings Name, Description
+   */
   constructor(
     @Inject(MAT_DIALOG_DATA)
     public data: {
       Name: string;
       Description: string;
     }
-  ) { }
-  ngOnInit(): void {
-  }
+  ) {}
+  ngOnInit(): void {}
 }
